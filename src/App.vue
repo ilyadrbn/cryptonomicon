a75e58529d16d9a64fa001e21f6f150eb697e04b
ilyadrbn <ilyadrbn@gmail.com>
2024-04-30 16:30:34 +0300

    fetch all coins and ajax when created

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div
-      v-if="false"
+      v-if="loadingState"
       class="w-100 h-100 fixed inset-0 z-50 flex items-center justify-center bg-purple-800 opacity-80"
     >
       <svg
@@ -36,17 +36,24 @@
                 type="text"
                 name="wallet"
                 id="wallet"
-                class="block w-full rounded-md border-gray-300 pr-10 text-gray-900 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+                class="mb-1 block w-full rounded-md border-gray-300 p-2 text-gray-900 focus:border-gray-500 focus:outline-none focus:ring-gray-500"
                 placeholder="Например DOGE"
                 v-model="ticker"
                 @keydown.enter="add"
+                @input="findCoin(), (showException = false)"
               />
             </div>
-            <div class="flex flex-wrap rounded-md bg-white p-1 shadow-md shadow-md">
-              <span
+            <div class="flex flex-wrap rounded-md bg-white p-1 shadow-md" v-if="ticker">
+              <!-- <span
                 class="m-1 inline-flex cursor-pointer items-center rounded-md bg-gray-300 px-2 text-xs font-medium text-gray-800"
                 v-for="p in pattern"
                 :key="p"
+                @click="addPattern(p)"
+              > -->
+              <span
+                class="m-1 inline-flex cursor-pointer items-center rounded-md bg-gray-300 px-2 text-xs font-medium text-gray-800"
+                v-for="(p, idx) in pattern"
+                :key="idx"
                 @click="addPattern(p)"
               >
                 {{ p }}
@@ -54,9 +61,10 @@
             </div>
           </div>
         </div>
+        <div v-if="showException" class="text-sm text-red-600">Такой тикер уже добавлен</div>
         <button
           type="button"
-          @click="add"
+          @click="add()"
           class="my-4 inline-flex items-center rounded-full border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium leading-4 text-white shadow-sm transition-colors duration-300 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
         >
           <svg
@@ -88,7 +96,9 @@
             @click="selected = t"
           >
             <div class="px-4 py-5 text-center sm:p-6">
-              <dt class="truncate text-sm font-medium text-gray-500">{{ t.name }} - USD</dt>
+              <dt class="truncate text-sm font-medium text-gray-500">
+                {{ t.name.toUpperCase() }} - USD
+              </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">{{ t.price }}</dd>
             </div>
             <div class="w-full border-t border-gray-200"></div>
@@ -116,7 +126,9 @@
         <hr class="my-4 w-full border-t border-gray-600" />
       </template>
       <section class="relative" v-if="selected && tickers.length">
-        <h3 class="my-8 text-lg font-medium leading-6 text-gray-900">{{ selected.name }} - USD</h3>
+        <h3 class="my-8 text-lg font-medium leading-6 text-gray-900">
+          {{ selected.name.toUpperCase() }} - USD
+        </h3>
         <div class="flex h-64 items-end border-b border-l border-gray-600">
           <!-- <div
             class="bg-purple-800 border w-10 h-24"
@@ -163,34 +175,76 @@ export default {
   name: 'App',
   data() {
     return {
+      loadingState: true,
+      showException: false,
       ticker: null, // поле ввода
-      pattern: ['BTC', 'DOGE', 'ETH', 'CHD'], // список доступных тикеров в шаблонах под инпутом
+      pattern: [], // список доступных тикеров в шаблонах под инпутом
       tickers: [], // массив, в которых пушим все цены
       selected: null, // выбранный тикер
-      graphStates: [] // состояния графика
+      graphStates: [], // состояния графика
+      fetchCoins: null
     };
   },
+  created() {
+    this.fetchCoins = (async () => {
+      const data = await fetch(`https://min-api.cryptocompare.com/data/all/coinlist?summary=true`);
+      const coins = await data.json();
+      this.loadingState = false;
+      return coins.Data;
+    })();
+
+    const tickersData = localStorage.getItem('cryptonomicon-list');
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach(ticker => { 
+        this.subscribeToUpdates(ticker.name);
+      })
+    }
+  },
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD`)
+          .then((res) => res.json())
+          .then((data) => {
+            this.tickers.find((t) => t.name === tickerName).price = data.USD;
+            this.graphStates.push(data.USD);
+          });
+      }, 3000);
+    },
+
+    findCoin() {
+      this.fetchCoins.then((coins) => {
+        for (const key in coins) {
+          if (coins[key].Symbol.startsWith(this.ticker.toUpperCase(), 0)) {
+            this.pattern.push(coins[key].Symbol);
+            if (this.pattern.length > 4) this.pattern.shift();
+          }
+        }
+      });
+    },
+    findInTable() {
+      return this.tickers.find((t) => t.name.toUpperCase() === this.ticker.toUpperCase());
+    },
     add() {
-      if (this.ticker) {
+      if (this.ticker && !this.findInTable()) {
         const currentTicker = {
           name: this.ticker,
           price: 0
         };
         this.tickers.push(currentTicker);
-        setInterval(async () => {
-          await fetch(
-            `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD`
-          )
-            .then((res) => res.json())
-            .then((data) => {
-              this.tickers.find((t) => t.name === currentTicker.name).price = data.USD;
-              this.graphStates.push(data.USD);
-            });
-        }, 3000);
+
+        localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
+
+        this.subscribeToUpdates(currentTicker.name);
+
         this.ticker = null;
+        this.showException = false;
+      } else if (this.findInTable) {
+        this.showException = true;
       }
     },
+
     /**
      * Normalize the graph states based on the maximum and minimum values.
      *
