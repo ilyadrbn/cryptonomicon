9d4d634e3d2053aec9aab697c84c9fa637e1af4c
Ilya Gorbul <ilyadrbn@gmail.com>
2024-07-31 15:37:05 +0300

    finish WS logic

--- src/api.js
+++ src/api.js
@@ -1,50 +1,55 @@
 const API_KEY = 'd3bbe919bf07cff37ac0ea6d81c1bfd010ed648112dc7061b8f3c5898e56aa79';
 
 const coinsHandlers = new Map(); // ! список функций, которые будут срабатывать при добавлении монетки
+const webSocket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`);
 
-const loadCoins = () => {
-  if (coinsHandlers.size === 0) {
+webSocket.addEventListener('message', (event) => {
+  const { TYPE: type, FROMSYMBOL: currencyCoin, PRICE: price } = JSON.parse(event.data);
+  if (type !== '5') {
     return;
   }
-  return fetch(
-    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[...coinsHandlers.keys()].join(
-      ','
-    )}&tsyms=USD&api_key=${API_KEY}`
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((rawData) => {
-      /*
-       ** rawData = {
-       **   DOGE: {
-       **     USD: 1000
-       **   }
-       ** }
-       **   => return  { DOGE: 1000 } where 1000 is price in USD
-       */
-      const updatedPrices = Object.fromEntries(
-        Object.entries(rawData).map(([key, value]) => [key, value.USD])
-      );
-      Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-        const handlers = coinsHandlers.get(currency) || [];
-        handlers.forEach((cb) => cb(newPrice));
-      });
-    });
+  const handlers = coinsHandlers.get(currencyCoin) || [];
+  handlers.forEach((cb) => cb(price));
+});
+
+const sendToWS = (message) => {
+  const stringifiedMessage = JSON.stringify(message);
+
+  if (webSocket.readyState === WebSocket.OPEN) {
+    webSocket.send(stringifiedMessage);
+    return;
+  }
+
+  webSocket.addEventListener(
+    'open',
+    () => {
+      webSocket.send(stringifiedMessage);
+    },
+    { once: true }
+  );
+};
+
+const subscribeToCoinWS = (coin) => {
+  sendToWS({
+    action: 'SubAdd',
+    subs: [`5~CCCAGG~${coin}~USD`]
+  });
+};
+
+const unsubscribeFromCoinWS = (coin) => {
+  sendToWS({
+    action: 'SubRemove',
+    subs: [`5~CCCAGG~${coin}~USD`]
+  });
 };
 
 export const subscribeToCoinUpdates = (coin, cb) => {
   const existingSubscribers = coinsHandlers.get(coin) || []; // ! получаем список раннее добавленных функций cb или []
   coinsHandlers.set(coin, [...existingSubscribers, cb]); // ! добавляет к coin новый "cb" в довесок к уже имеющимся функциям existingSubscribers
+  subscribeToCoinWS(coin);
 };
 
 export const unsubscribeFromCoinUpdates = (coin) => {
   coinsHandlers.delete(coin.toUpperCase());
-  // const existingSubscribers = coinsHandlers.get(coin) || []; // ! получаем список раннее добавленных функций cb или []
-  // coinsHandlers.set(
-  //   coin,
-  //   existingSubscribers.filter((sub) => sub !== cb) // ! фильтруем для coin колбэки отличные от колбэка "cb"
-  // );
+  unsubscribeFromCoinWS(coin.toUpperCase());
 };
-
-setInterval(loadCoins, 5000);
